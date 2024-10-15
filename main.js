b09db09029ef10570b8e5748e892007857650df0
Samandar <zafarovsamandar5444@gmail.com>
2024-10-15 15:02:29 +0500

    12345678

---
+++ main.js
@@ -0,0 +1,175 @@
+// const a = 5
+
+// QOSHIMCHA
+
+// let kvadratAtomoni= prompt("kvadrat A tomonini kiriting");
+
+// let kvadratYuzasi = kvadratAtomoni * kvadratAtomoni;
+// let kvadratPeremetri = +kvadratAtomoni + +kvadratAtomoni + +kvadratAtomoni + +kvadratAtomoni;
+
+// console.log( "peremetri" +" "  + kvadratPeremetri);
+// console.log( "yuzasi" +" "  + kvadratYuzasi );
+
+// QOSHIMCHA
+
+
+
+// QOSHIMCHA
+
+// const a = 5
+// console.log(a*a*3.14);
+// console.log(2*a*3.14);
+
+// QOSHIMCHA
+
+
+
+
+
+
+
+// 1MISOL  uy ishi
+
+// let uzunligi = prompt("uzunligini kiriting")  ;
+// let uzunlik = parseFloat(uzunligi) ;
+// let sm=parseInt(uzunlik/100);
+// alert(sm +" "+ "metr");
+
+// 1MISOL  uy ishi
+
+// 2MISOL  uy ishi
+
+// let kg = prompt("kg ni kiriting")  ;
+// let tonna = parseFloat(kg) ;
+// let t=parseInt(tonna/1000);
+// alert( t +" "+ "tonna");
+
+// 2MISOL  uy ishi
+
+// 3MISOL  uy ishi
+
+// let bayt = prompt("bayt ni kiriting")  ;
+// let kb = parseFloat(bayt) ;
+// let k=parseInt(kb/1024);
+// alert( k +" "+ "kb");
+
+// 3MISOL  uy ishi
+
+// 4MISOL  uy ishi
+
+// let a= +prompt("A-sonini kirgizing");
+// let b= +prompt("B-sonini kirgizing");
+
+// let c =a%b;
+
+// alert(c);
+
+// 4MISOL  uy ishi
+
+// 5MISOL  uy ishi
+
+// let a= +prompt("A-sonini kirgizing");
+// let b= +prompt("B-sonini kirgizing");
+
+// let c =a/b;
+
+// alert(`a kesmada b kesmani ${c} martta joylashtirish mumkun   `);
+
+// 5MISOL  uy ishi
+
+// 6MISOL  uy ishi
+
+// let a = prompt("2 honali son kiritin");
+
+// let e = parseInt(a/10);
+
+// let d = a % 10;
+
+// alert(` onliklar xonasi ${e} birliklar xonasi ${d}  `);
+
+// 6MISOL  uy ishi
+
+// 7MISOL  uy ishi
+
+// let a = prompt("2 honali son kiritin");
+
+// let e = parseInt(a/10);
+
+// let d = a % 10;
+
+// let b= e + d;
+
+// alert(`yigindisi ${b}  `);
+
+// 7MISOL  uy ishi
+
+// 8MISOL  uy ishi
+
+// let a = prompt("2 honali son kiritin");
+
+// let e = parseInt(a/10);
+
+// let d = a % 10;
+
+// alert(`${d}${e} `);
+
+// 8MISOL  uy ishi
+
+// 9MISOL  uy ishi
+
+// let a = prompt("3 honali son kiritin");
+
+// let e = parseInt(a/100);
+
+// alert(`${e} `);
+
+// 9MISOL  uy ishi
+
+// 10MISOL  uy ishi
+
+// let a = prompt("3 honali son kiritin");
+// let c = a / 100;
+// let e = parseInt(a/10%10);
+// let d = a % 10;
+
+// alert(`birliklar xonasi ${d}  onliklar xonasi ${e}   `);
+
+// 10MISOL  uy ishi
+
+// 11MISOL  uy ishi
+
+// let a = prompt("3 honali son kiritin");
+// let c =parseInt( a / 100);
+// let e = parseInt(a/10%10);
+
+// let d = a % 10;
+
+// let b = e + d + c;
+
+// alert("yigindisi" +" " + b);
+
+// 11MISOL  uy ishi
+
+// 12MISOL  uy ishi
+
+// let a = prompt("3 honali son kiritin");
+// let c =parseInt( a / 100);
+// let e = parseInt(a/10%10);
+
+// let d = a % 10;
+
+// alert(` hosil bolgan son ${d}${e}${c} ` );
+
+// 12MISOL  uy ishi
+
+// 13MISOL  uy ishi
+
+// let a = prompt("3 honali son kiritin");
+// let c =parseInt( a / 100);
+// let e = parseInt(a/10%10);
+
+// let d = a % 10;
+
+// alert(` hosil bolgan son ${e}${d}${c} ` );
+
+// 13MISOL  uy ishi
